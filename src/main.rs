096361b2050167d54923c6d0d25d085a3d4097ed
Andrey Ivanov <lekkion@gmail.com>
2016-03-21 21:44:15 +0400

    done load

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,21 @@ use std::fs::File;
 use std::io::{BufWriter, BufReader};
 use std::io::prelude::*;
 
+enum Src {
+    None,
+    Path(&'static str),
+}
 
 
-#[derive(Debug)]
 struct Mvm {
     source: &'static str,
-    file: 
+    file: Src
 }
 
 trait Genetare {
-	fn get_source(&self);
+	fn get_source(&mut self);
+
+	fn console(&self);
 }
 
 impl Genetare for Mvm {
@@ -37,36 +42,46 @@ impl Genetare for Mvm {
 		}
 
 		let vec: Vec<&str> = self.source.split('/').collect();
-		self.file = vec[vec.len()-1];		
+		self.file = Src::Path(vec[vec.len()-1]);		
 
-    // 	let mut file = File::create("temp.pbf").unwrap();
-	   //  let mut buffer_write = BufWriter::new(file);
-	   //  let mut buffer_read = BufReader::new(responce);
-	   //  let mut download: u64 = 0;   	    	
+    	let mut file = File::create(vec[vec.len()-1]).unwrap();
+	    let mut buffer_write = BufWriter::new(file);
+	    let mut buffer_read = BufReader::new(responce);
+	    let mut download: u64 = 0;   	    	
 
-	   //  while  download != size {
-	   //  	let length = {	
-	   //  		let buffer = buffer_read.fill_buf().unwrap();	
+	    while  download != size {
+	    	let length = {	
+	    		let buffer = buffer_read.fill_buf().unwrap();	
 	    		
-	   //  		buffer_write.write(buffer).unwrap();
-				// buffer_write.flush().unwrap();
+	    		buffer_write.write(buffer).unwrap();
+				buffer_write.flush().unwrap();
 
-				// buffer.len()
-	   //  	};
+				buffer.len()
+	    	};
 
-	   //  	buffer_read.consume(length);
-	   //  	println!("{:?}", length);
-	   //  	download += length as u64;
-	   //  }
+	    	buffer_read.consume(length);
+	    	println!("{:?}", length);
+	    	download += length as u64;
+	    }
+	}
+
+	fn console(&self) {
+		match self.file {
+		    Src::Path(src) => println!("{:?}", src),
+		    Src::None => println!("None"),
+		}
 	}
 }
 
 static CRIMEAN: &'static str = "http://download.geofabrik.de/russia/crimean-fed-district-latest.osm.pbf";
 
 fn main() {    
-    let central = Mvm {
-    	source: CRIMEAN
+    let mut central = Mvm {
+    	source: CRIMEAN,
+    	file: Src::None
     };
 
     central.get_source();
+
+    central.console(); 
 }
